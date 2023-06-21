f9093d6143b4194a53715b39995c573312694fa4
rodneyvictorsoares <123011008+rodneyvictorsoares@users.noreply.github.com>
2023-06-21 20:50:45 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,23 @@
+function multiplicateNumbers(a, b) {
+  return a * b;
+}
+
+function sumNumbers(a, b) {
+  return a + b;
+}
+
+function divideNumbers(a, b) {
+    return a / b;
+}
+
+function substractNumbers(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    multiplicateNumbers,
+    sumNumbers,
+    divideNumbers,
+    substractNumbers
+}
+
